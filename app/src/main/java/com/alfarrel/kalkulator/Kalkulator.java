e0b49a8f7f7723095ee9ff77a7913a934878b499
Alfarrel Indrawan <alfarrelindrawan@gmail.com>
2020-11-07 15:14:19 +0700

    Pembuatan readme dan file lain

--- app/src/main/java/com/alfarrel/kalkulator/Kalkulator.java
+++ app/src/main/java/com/alfarrel/kalkulator/Kalkulator.java
@@ -1,2 +1,17 @@
-package com.alfarrel.kalkulator;public class Kalkulator {
+package com.alfarrel.kalkulator;
+
+import java.util.Scanner;
+
+public class Kalkulator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+    }
+    int num1,num2;
+    void Kalkulator() {
+
+    }
+    void tampilkan() {
+        System.out.println();
+    }
 }
--- app/src/test/java/com/alfarrel/kalkulator/testKalkulator.java
+++ app/src/test/java/com/alfarrel/kalkulator/testKalkulator.java
@@ -1,2 +1,11 @@
-package com.alfarrel.kalkulator;public class testKalkulator {
+package com.alfarrel.kalkulator;
+
+import org.junit.Test;
+
+public class testKalkulator {
+    @Test
+    void testTampilkan() {
+        Kalkulator kal = new Kalkulator();
+        assertvalue("0.0",kal.tampilkan());
+    }
 }
